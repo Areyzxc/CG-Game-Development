899c1309509a283d60a2b613eb2cd88bceabfc87
James Aries <187737142+Areyzxc@users.noreply.github.com>
2025-10-28 22:17:57 +0800

    Update admin_profile.php

--- admin_profile.php
+++ admin_profile.php
@@ -213,9 +213,16 @@ $stats = $stmt->fetch(PDO::FETCH_ASSOC);
 
 <?php include 'includes/admin_footer.php'; ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap JS Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<!-- Custom JS -->
+<script>
+    // Make bootstrap available globally
+    const { Tooltip, Dropdown } = bootstrap;
+</script>
 <script src="assets/js/admin_global.js"></script>
-<script src="assets/js/admin_profile.js"></script>
+<script src="assets/js/admin_profile.js" defer></script>
 
 </body>
 </html>
